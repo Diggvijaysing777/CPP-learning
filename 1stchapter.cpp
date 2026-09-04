58d0851fc44665abacc09bbce02cd275dea4b69c
digvijay <diggvijaysing@gmail.com>
2026-09-04 23:44:08 +0530

    2nd day of learning

---
+++ 1stchapter.cpp
@@ -0,0 +1,21 @@
+// //wap to print the sum of two numbers 
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int first_number ;
+//     int second_number ;
+//     cout << "enter first number";
+//     cin >> first_number ;
+//     cout << "enter 2nd number";
+//     cin >> second_number;
+//     int sum = first_number + second_number ;
+//     cout << sum ;
+//     return 0 ;
+// }
+#include <iostream>
+using namespace std;
+int main(){
+    const int num = 45;
+    cout<<"number is = "<< num;
+    return 0 ;
+}
