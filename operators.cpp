47aa352564b5739c462971570bb311fefd4de930
digvijay <diggvijaysing@gmail.com>
2026-09-06 10:37:44 +0530

    solve questions based  on operator logics

--- operators.cpp
+++ operators.cpp
@@ -40,15 +40,58 @@
 
 // }
 //DIVISION
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    double a,b ;
+//     std::cout<<"enter a ";
+//     std::cin>>a;
+//     std::cout<<"enter b";
+//     std::cin>>b;
+//  double div = a/b;
+//     std::cout<<div;
+//     return 0;
+// }
+//WAP to find remainder when one number is divides by other 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//    int a , b;
+//    cout << "enter a";
+//    cin >> a;
+//    cout << "enter b";
+//    cin >> b;
+//    int remainder = a % b;
+//    cout << remainder;
+//    return 0;
+// }
+//WAP TO FIND WHEATHER NUMBER IS ODD OR EVEN 
+// #include <iostream>
+// using namespace std ;
+// int main (){
+//    int a ;
+//    cout << "enter your number ";
+//    cin >> a ;
+// if ( a % 2 == 0 ){
+//    cout << "number is odd";
+// }else{
+//    cout << " number is even";
+// }
+// return 0 ;
+// }
+//wap to find the greater of two number
 #include<iostream>
 using namespace std;
-int main(){
-   double a,b ;
-    std::cout<<"enter a ";
-    std::cin>>a;
-    std::cout<<"enter b";
-    std::cin>>b;
- double div = a/b;
-    std::cout<<div;
-    return 0;
+int main (){
+   int a , b ;
+   cout << "enter first number ";
+   cin >> a ;
+   cout << "enter 2nd number";
+   cin >> b ;
+   if (a > b  ){
+      cout << "greater no is  "<< a ;
+   }else{
+      cout << "greater no is "<< b ;
+   }
+   return 0 ;
 }
