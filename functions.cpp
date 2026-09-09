bc8edad3519e3dd55be2c45566349931a26a0018
digvijay <diggvijaysing@gmail.com>
2026-09-09 12:54:49 +0530

    functions in cpp

---
+++ functions.cpp
@@ -0,0 +1,25 @@
+//wap to print sum of two numbers using add function
+// #include<iostream>
+// using namespace std;
+// int add(int a , int b){
+//     return a+b ;
+// }
+// int main(){
+//     int num1, num2;
+//     cout << "enter num1 :";
+//     cin >> num1;
+//     cout << "enter num2 :";
+//     cin >> num2;
+//     cout << "num1 + num2 = :"<<add(num1 , num2);
+//     return 0;
+// }
+#include<iostream>
+#include<string>
+using namespace std;
+string greet(string hello ){
+    return hello ;
+}
+int main (){
+    cout << greet("Hello");
+    return 0;
+}
