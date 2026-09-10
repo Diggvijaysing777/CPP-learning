c1bdc6e81561bb2290c0036dc87349649d76b16b
digvijay <diggvijaysing@gmail.com>
2026-09-10 11:07:19 +0530

    all basic functions questions solved

--- functions.cpp
+++ functions.cpp
@@ -36,17 +36,53 @@
 //     return 0;
 // }
 //wap take two integers and make add() and print their sum 
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int add(int a , int b){
+//     return a+b ;
+// }
+// int main (){
+//     int num1 , num2 ;
+//     cout << "enter num1 = ";
+//     cin >> num1 ;
+//     cout << "enter num2 = ";
+//     cin >>num2 ;
+//     cout << "sum of num1 and num2 = "<< add(num1 , num2);
+//     return 0 ;
+// }
+//Write  functon even odd check number and tells wheather no is even or odd
+// #include<iostream>
+// using namespace std ;
+// int evenOdd(int a){
+//     return a%2 ;
+// }
+// int main (){
+//     int num1 ;
+//     cout << "enter the number = ";
+//     cin >> num1 ;
+//     if(evenOdd(num1) == 0 ){
+//         cout << "number is even";
+//     }else{
+//         cout << "number is odd";
+//     }
+//     return 0 ;
+// }
+// write a function maximum() takes input two number and print larger number 
+#include <iostream>
 using namespace std;
-int add(int a , int b){
-    return a+b ;
+int maximum(int a , int b ){
+    return a>b ;
 }
-int main (){
-    int num1 , num2 ;
-    cout << "enter num1 = ";
+int main(){
+    int num1 , num2;
+    cout << "enter 1st no =";
     cin >> num1 ;
-    cout << "enter num2 = ";
-    cin << num2 ;
-    cout << "sum of num1 and num2 = "<< add(num1 , num2);
+    cout << "enter 2nd no =";
+    cin >> num2;
+    if(maximum(num1 , num2)==1){
+        cout << " larger number is = "<< num1;
+    }else{
+        cout << "larger number is = " << num2;
+    }
     return 0 ;
 }
