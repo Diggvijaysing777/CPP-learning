e08933008bc6949216da9d2e53daf25375e5110d
digvijay <diggvijaysing@gmail.com>
2026-09-10 13:47:07 +0530

    solve inter lever function ques

--- functions.cpp
+++ functions.cpp
@@ -68,21 +68,49 @@
 //     return 0 ;
 // }
 // write a function maximum() takes input two number and print larger number 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+// int maximum(int a , int b ){
+//     return a>b ;
+// }
+// int main(){
+//     int num1 , num2;
+//     cout << "enter 1st no =";
+//     cin >> num1 ;
+//     cout << "enter 2nd no =";
+//     cin >> num2;
+//     if(maximum(num1 , num2)==1){
+//         cout << " larger number is = "<< num1;
+//     }else{
+//         cout << "larger number is = " << num2;
+//     }
+//     return 0 ;
+// }
+//Write a function add() that takes two integers and returns their sum. Print the returned value in main()
+// #include<iostream>
+// using namespace std;
+// int add(int a , int b){
+//     return a+b;
+// }
+// int main (){
+//     int num1 , num2;
+//     cout << "enter 1st number = ";
+//     cin >> num1;
+//     cout << "enter 2nd number = ";
+//     cin >> num2;
+//     cout << " sum of both numbers = "<< add(num1 , num2);
+//     return 0 ;
+// }
+//Write a function cube() that takes an integer and returns its cube.
+#include<iostream>
 using namespace std;
-int maximum(int a , int b ){
-    return a>b ;
+int cube(int a ){
+    return a*a*a ;
 }
 int main(){
-    int num1 , num2;
-    cout << "enter 1st no =";
+    int num1 ;
+    cout << "Enter the no " ;
     cin >> num1 ;
-    cout << "enter 2nd no =";
-    cin >> num2;
-    if(maximum(num1 , num2)==1){
-        cout << " larger number is = "<< num1;
-    }else{
-        cout << "larger number is = " << num2;
-    }
+    cout << "Cube of this no = "<<cube(num1);
     return 0 ;
 }
